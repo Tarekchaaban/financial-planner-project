2d427339886e8a8331c4c907bc6b73c35c11389d
Tarek Chaaban <Chaaban_tarek@yahoo.com>
2023-03-24 01:50:09 +0000

    completed lesson-11.

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,39 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-console */
 /* eslint-disable prefer-const */
+
+// classes
+class Invoice {
+  client: string;
+  details: string;
+  amount: number;
+
+  constructor(c: string, d: string, a: number) {
+    this.client = c;
+    this.details = d;
+    this.amount = a;
+  }
+
+  format() {
+    return `${this.client} owes $${this.amount} for ${this.details};`
+  }
+}
+
+const invOne = new Invoice('mario', 'work on the mario website', 250);
+const invTwo = new Invoice('luigi', 'work on the luigi website', 300);
+
+let invoices: Invoice[] = [];
+invoices.push(invOne);
+invoices.push(invTwo);
+
+console.log(invoices);
+
 const anchor = document.querySelector('a')!;
 
-console.log(anchor.href);
 
 // const form = document.querySelector('form')!;
 
 const form = document.querySelector('.new-item-form') as HTMLFormElement;
-// console.log(form.children);
 
 
 // inputs
