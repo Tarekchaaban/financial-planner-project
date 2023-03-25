58533296dd470a42bd8654856199c26d659e30ae
Tarek Chaaban <Chaaban_tarek@yahoo.com>
2023-03-25 00:57:13 +0000

    completed lesson 14.

--- public/app.js
+++ public/app.js
@@ -1,19 +1,7 @@
-'use strict';
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-console */
 /* eslint-disable prefer-const */
-// classes
-class Invoice {
-  constructor(client, details, amount) {
-    this.client = client;
-    this.details = details;
-    this.amount = amount;
-  }
-
-  format() {
-    return `${this.client} owes $${this.amount} for ${this.details};`;
-  }
-}
+import { Invoice } from './classes/Invoice.js';
 const invOne = new Invoice('mario', 'work on the mario website', 250);
 const invTwo = new Invoice('luigi', 'work on the luigi website', 300);
 let invoices = [];
